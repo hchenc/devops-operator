e13de975600af5a5db4e47f1c54dc3f2ad32ae02
hchenc <happytime_ch@foxmail.com>
2021-12-01 09:51:39 +0800

    fix bug on application assemble

--- pkg/constant/const.go
+++ pkg/constant/const.go
@@ -7,6 +7,8 @@ const (
 
 	KubesphereAppType     = "app.kubernetes.io/type"
 	KubesphereAppName     = "app.kubernetes.io/name"
+	KubesphereVersion     = "version"
+	KubesphereInitVersion = "v1"
 	KubesphereDescription = "kubesphere.io/description"
 	KubesphereWorkspace   = "kubesphere.io/workspace"
 	KubesphereCreator     = "kubesphere.io/creator"
